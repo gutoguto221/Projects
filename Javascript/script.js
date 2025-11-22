32a2c1fad57195c8ab495eae7e130f296b9c295e
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-11-21 23:11:02 -0300

    mudança de nome

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,29 @@
-const elements = document.querySelectorAll(".button")
 
-console.log (elements)
+
+//const elements = document.querySelector(".main-button")
+
+//console.log (elements)
+
+/*
+console.log(element.textContent) // SÓ HTML
+console.log(element.innerText)  // LEVA EM CONTA O CSS
+console.log(element.innerHTML) // TRÁS TUDO e PERMITE ADICIONAR HTML diretamente
+*/
+
+//Eventos --> Sempre começam com o comando ON
+
+
+const input = document.querySelector("#main-input")
+const paragrapho = document.querySelector("paragraph-js")
+
+
+function cliqueiNoBotao(){
+    paragrapho.textContent= input.value
+
+}
+function digiteiNoInput(){
+    console.log(input.value)
+    
+}
+
+
