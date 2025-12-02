4c3dc45fd1007230944bfc2756780626b6ec1434
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-02 02:54:46 -0300

    Estudo de else if

---
+++ Javascript/else-if.js
@@ -0,0 +1,14 @@
+/*  IF = SE
+    ELSE = SENÃO
+    ELSE IF = Pode usar quantos quiser
+*/
+
+const temperatura = 30
+
+if(temperatura === 36){
+    console.log ("Saudável")
+    } else if (temperatura >36){
+        console.log ("Febre")
+    } else{
+        console.log("Hipotermia")
+    }
