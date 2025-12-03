aa42e7ff8f3aeb4f8155bc2eab31c3411d1144ab
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-03 00:10:38 -0300

    Switch Case

---
+++ Javascript/switchcase.js
@@ -0,0 +1,29 @@
+/* Switch Case - Controlador de Fluxo */
+
+const temperature = 30
+
+switch (temperature) {
+    case 10:
+        console.log("Tá meio frio")
+        break
+
+    case 20:
+        console.log("Temperatura ambiente")
+        break
+
+    case 30:
+        console.log("Tá meio quente")
+        break
+
+    case 40:
+        console.log("Tá QUENTE")
+        break
+
+    case 50:
+        console.log("Alerta de ensolação")
+        break
+
+    default:
+        console.log ("Não sei")
+        break 
+}
