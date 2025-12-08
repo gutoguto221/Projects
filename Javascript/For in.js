c4f2ae6d8a2829cf3adf0b3c347cc11b1d9e7e1c
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-07 23:26:56 -0300

    For in

---
+++ Javascript/For in.js
@@ -0,0 +1,7 @@
+const users = {name: 'Guto', age: 38, street:"Asian Street"}
+
+for (const key in users){
+
+}
+
+//Pega apenas a chave dos elementos, ou seja: name, age e street apenas, não os valores
