066603c92850790cf503eb75807b06be77369ac5
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-10 00:09:00 -0300

    Function Calculadora + Return

---
+++ Javascript/Functions Calculadora.js
@@ -0,0 +1,26 @@
+
+function sum(value, value2){
+    console.log(value + value2)
+}
+
+sum(2, 2)
+
+
+function sub(value, value2){
+   console.log(value - value2)
+}
+
+sub(100,5)
+
+function div(value, value2){
+    console.log(value / value2)
+}
+
+div(10,2)
+
+function mult(value, value2){
+  console.log(value * value2)
+}
+
+mult(7,8)
+
---
+++ Javascript/Return.js
@@ -0,0 +1,7 @@
+function sum(value, value2){
+    const result = value + value2
+    return result
+}
+
+const myResult = sum(2000, 50)
+console.log(myResult + " é o resultado")
