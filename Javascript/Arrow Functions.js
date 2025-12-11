1b3ff6e8dbccb664d0e0bbd255b9e6958cd88182
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-11 01:25:44 -0300

    Arrow Function 1

---
+++ Javascript/Arrow Functions.js
@@ -0,0 +1,10 @@
+
+//Essas duas estruturas abaixo fazem a MESMA coisa
+
+function sum1 (number1, number2){
+    return number1 + number2
+}
+
+const sum = (number1,number2) => number1 + number2
+
+console.log(sum(3,4))
