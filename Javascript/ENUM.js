36b1c737fbad44f552f8a2baa4734568aca526c6
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-15 04:39:24 -0300

    ENUM

---
+++ Javascript/ENUM.js
@@ -0,0 +1,14 @@
+/* Uma variável que armazena um valor que tem duas características: todas as letras em caps lock 
+separadas por um underline e que armazenam um valor que pode ser replicado */
+
+const GAME_OPTIONS = {
+VARIAVEL1 = 'a'
+VARIAVEL2 = 'b'
+VARIAVEL3 = 'c'
+
+}
+
+Aplicabilidade:
+
+GAME_OPTIONS.a 
+
