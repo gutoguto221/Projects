88699fac82a21f4f0d82edc0591496ad7ca91f37
gutoguto221 <augusto_cristino@yahoo.com.br>
2025-12-19 16:26:25 -0300

    Estudo Reduce

---
+++ Javascript/Reduce.js
@@ -0,0 +1,16 @@
+const cart =[
+
+{ productName: 'Abóbora', pricePerKg: 5, kg: 1},
+{ productName: 'Pepino', pricePerKg: 3.55, kg: 1.3},
+{ productName: 'Limão', pricePerKg: 1.2, kg: 2},   
+{ productName: 'Abacate', pricePerKg: 5.4, kg: 1.67},
+{ productName: 'Morango', pricePerKg: 11.9, kg: 3},
+
+]
+
+const finalValue = cart.reduce((acc, value) => {
+    const result = value.pricePerKg * value.kg
+return acc + result
+},0)
+
+console.log(`Total da compra: R$ ${finalValue.toFixed(2)}`)
