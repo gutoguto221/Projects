1156d6bf14d592c32705c29e90b9e80edf68ba8a
gutoguto221 <augusto_cristino@yahoo.com.br>
2026-01-02 04:02:39 -0300

    DevBurger com ForEach

--- projeto-devburger/script.js
+++ projeto-devburger/script.js
@@ -1,8 +1,17 @@
-const menuOptions = [
-    {name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg'},
-    {name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png'},
-    {name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png'},
-    {name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png'},
-    {name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png'},
-    {name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png'},
-]
+const list = document.querySelectorAll('ul')
+const buttonShowAll = document.querySelector('.show-all')
+let myLi = ''
+
+function showAll() {
+    menuOptions.forEach((product) => {
+        myLi += `
+            <li>
+                <img src="${product.image}" alt="${product.title}">
+                <p>${product.title}</p>
+                <p class="item-price">R$ ${product.price}</p>
+            </li>
+        `
+    })
+    list.innerHTML = myLi
+}
+buttonShowAll.addEventListener('click', showAll)
