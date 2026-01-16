6b8c39724acd795b1a27a04e73b1cfb69f4c5b2f
gutoguto221 <augusto_cristino@yahoo.com.br>
2026-01-16 16:47:53 -0300

    Node

--- Estudo NODE/server.js
+++ Estudo NODE/server.js
@@ -1,9 +1,19 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) => {
-    res.send('Bem-vindo à rota de usuários!')
+app.get('/usuarios/:id', (req, res) => {
+    console.log(req)
+    
+    res.send("Rota de usuário")
+})
+
+app.post('/usuarios', (req, res) => {
+    use.push(req.body)
+
+    res.status(201).json({ message: 'Usuário criado com sucesso!' })
+
 })
 
 app.listen(3000)
