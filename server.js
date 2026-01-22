3a0583bcc581ff9fa4d5e8bd50eb70ded92084aa
gutoguto221 <augusto_cristino@yahoo.com.br>
2026-01-21 21:11:47 -0300

    Node e Prisma 2

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
 const users = []
 
-app.get('/usuarios', (req, res) => {
-    console.log(req)
+app.get('/usuarios', async (req, res) => {
+    const users = await prisma.user.findMany()
 
     res.status(200).json(users)
 })
 
-app.post('/usuarios', (req, res) => {
-  users.push(req.body) 
+app.post('/usuarios', async (req, res) => {
+  
+  
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name
+    } 
+  })
 
     res.status(201).json({message: "Usuário criado com sucesso"})
 })
